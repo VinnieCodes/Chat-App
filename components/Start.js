4fa22d507f20c81d05944b65dce10ec802f37b88
Vinnie <vincentathorne2005@gmail.com>
2024-07-22 10:16:56 -0700

    commit

--- components/Start.js
+++ components/Start.js
@@ -1,21 +1,34 @@
 import { useState } from "react";
-import { StyleSheet, View, Text, Button, TextInput } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  TextInput,
+  ImageBackground,
+} from "react-native";
 
 const Start = ({ navigation }) => {
   const [name, setName] = useState("");
   return (
     <View style={styles.container}>
-      <Text>Connect to Chat</Text>
-      <TextInput
-        style={styles.textInput}
-        value={name}
-        onChangeText={setName}
-        placeholder="Your Name"
-      />
-      <Button
-        title="Start Chatting"
-        onPress={() => navigation.navigate("Chat", { name: name })}
-      />
+      <ImageBackground
+        source={require("../img/BGimg.png")}
+        style={styles.imageBackground}
+      >
+        <Text>Connect to Chat</Text>
+        <Text>Connect to Chat</Text>
+        <TextInput
+          style={styles.textInput}
+          value={name}
+          onChangeText={setName}
+          placeholder="Your Name"
+        />
+        <Button
+          title="Start Chatting"
+          onPress={() => navigation.navigate("Chat", { name: name })}
+        />
+      </ImageBackground>
     </View>
   );
 };
@@ -25,6 +38,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  imageBackground: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+    width: "100%",
   },
   textInput: {
     width: "88%",
