522ab5ea233f0636d9d0b019f33155c2e68243ee
Vinnie <vincentathorne2005@gmail.com>
2024-07-29 15:50:31 -0700

    login and messages

--- components/Chat.js
+++ components/Chat.js
@@ -1,14 +1,19 @@
 import { useState, useEffect } from "react";
 import { StyleSheet, View, Platform, KeyboardAvoidingView } from "react-native";
-import { Bubble, GiftedChat } from "react-native-gifted-chat";
+import { Bubble, GiftedChat, InputToolbar } from "react-native-gifted-chat";
+import {
+  collection,
+  addDoc,
+  onSnapshot,
+  query,
+  orderBy,
+} from "firebase/firestore";
 
-const Chat = ({ route, navigation }) => {
-  const { name, background } = route.params;
+const Chat = ({ route, navigation, db }) => {
+  const { username, background, userID } = route.params;
   const [messages, setMessages] = useState([]);
   const onSend = (newMessages) => {
-    setMessages((previousMessages) =>
-      GiftedChat.append(previousMessages, newMessages)
-    );
+    addDoc(collection(db, "messages"), newMessages[0]);
   };
 
   const renderBubble = (props) => {
@@ -27,29 +32,64 @@ const Chat = ({ route, navigation }) => {
     );
   };
 
-  useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: "Hello developer",
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: "React Native",
-          avatar: "https://placeimg.com/140/140/any",
-        },
-      },
-      {
-        _id: 2,
-        text: "This is a system message",
-        createdAt: new Date(),
-        system: true,
-      },
-    ]);
-  }, []);
+  const renderInputToolbar = (props) => {
+     return <InputToolbar {...props} />;
+
+  };
+
+  // useEffect(() => {
+  //   setMessages([
+  //     {
+  //       _id: 1,
+  //       text: "Hello developer",
+  //       createdAt: new Date(),
+  //       user: {
+  //         _id: 2,
+  //         name: "React Native",
+  //         avatar: "https://placeimg.com/140/140/any",
+  //       },
+  //     },
+  //     {
+  //       _id: 2,
+  //       text: "This is a system message",
+  //       createdAt: new Date(),
+  //       system: true,
+  //     },
+  //   ]);
+  // }, []);
+  let unsubMessages;
 
   useEffect(() => {
-    navigation.setOptions({ title: name });
+    
+      // unregister current onSnapshot() listener to avoid registering multiple listeners when
+      // useEffect code is re-executed.
+      if (unsubMessages) unsubMessages();
+      unsubMessages = null;
+      navigation.setOptions({ title: username });
+      // Create a query to get the "messages" collection from the Firestore database
+      const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+      // This function will be called whenever there are changes in the collection.
+      unsubMessages = onSnapshot(q, (docs) => {
+        let newMessages = [];
+        // Iterate through each document in the snapshot
+        docs.forEach((doc) => {
+          newMessages.push({
+            id: doc.id,
+            ...doc.data(),
+            createdAt: new Date(doc.data().createdAt.toMillis()),
+          });
+        });
+        setMessages(newMessages);
+      });
+
+    // Clean up code
+    return () => {
+      if (unsubMessages) unsubMessages();
+    };
+  });
+
+  useEffect(() => {
+    navigation.setOptions({ title: username });
   }, []);
 
   return (
@@ -57,16 +97,18 @@ const Chat = ({ route, navigation }) => {
       <GiftedChat
         messages={messages}
         renderBubble={renderBubble}
+        renderInputToolbar={renderInputToolbar}
         onSend={(messages) => onSend(messages)}
         user={{
-          _id: 1,
+          _id: userID,
+          name: username,
         }}
       />
       {Platform.OS === "android" || Platform.OS === "ios" ? (
         <KeyboardAvoidingView behavior="height" />
       ) : null}
     </View>
-  );     
+  );
 };
 
 const styles = StyleSheet.create({
